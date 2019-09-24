2a6c79af0259ebbf243a921a6caeb7578825976d
N. Taylor Mullen <nimullen@microsoft.com>
2019-09-24 16:44:59 -0700

    Add 1.0 project

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class FooTagHelper : TagHelper, ITagHelper
+    {
+        Task ITagHelper.ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- Helper/EmailTagHelper.cs
+++ Helper/EmailTagHelper.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
 
 namespace Helper
 {
     [HtmlTargetElement("email", TagStructure = TagStructure.WithoutEndTag)]
-    public class EmailTagHelper : TagHelper
+    public class EmailTagHelper : TagHelper, ITagHelper
     {
+        Task ITagHelper.ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            return Task.CompletedTask;
+        }
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "a";
