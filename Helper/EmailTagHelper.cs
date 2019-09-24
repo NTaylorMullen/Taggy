9875b649186a36f192b23ec97a20bcec48bf7f71
Muchiachio <aldis.rameika@gmail.com>
2019-09-24 15:12:31 +0300

    Initial commit

---
+++ Helper/EmailTagHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Helper
+{
+    [HtmlTargetElement("email", TagStructure = TagStructure.WithoutEndTag)]
+    public class EmailTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+        }
+    }
+}
