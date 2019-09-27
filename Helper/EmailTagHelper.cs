b6aec1a41a7757cf0329f671ef216872eec4cb74
N. Taylor Mullen <nimullen@microsoft.com>
2019-09-27 10:34:22 -0700

    Fix EmailTagHelper

--- Helper/EmailTagHelper.cs
+++ Helper/EmailTagHelper.cs
@@ -6,11 +6,6 @@ namespace Helper
     [HtmlTargetElement("email", TagStructure = TagStructure.WithoutEndTag)]
     public class EmailTagHelper : TagHelper, ITagHelper
     {
-        Task ITagHelper.ProcessAsync(TagHelperContext context, TagHelperOutput output)
-        {
-            return Task.CompletedTask;
-        }
-
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "a";
